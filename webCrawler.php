4b55076e1d6b8fa8c0684093023c0cdcf101b307
Ariel Montero Monestel <arielmmonestel@gmail.com>
2016-05-21 17:02:09 -0600

    #todos

--- webCrawler.php
+++ webCrawler.php
@@ -2,7 +2,7 @@
 
 $arrayHTMLS = array();
 ini_set('memory_limit', '-1');
-
+$myfile = fopen("answer.txt", "w");
 
 
 
@@ -20,7 +20,9 @@ function getStartLinks($direction){
 
 
 function getHTML($url){
-	$textoHTML = "";
+	global $myfile;
+	$textoHTML = "[".$url."]";
+	fwrite($myfile, "[".$url."]");
 	//create object
 	$arrContextOptions=array(
     "ssl"=>array(
@@ -34,64 +36,119 @@ function getHTML($url){
 
 	$titles = $xpath->evaluate("/html//title");
 	for ($i = 0; $i < $titles->length; $i++) {
+
 		$title = $titles->item($i);
 			
-			$textoHTML.= $title->nodeValue;
+			//$textoHTML= $textoHTML."[".$title->nodeValue."]";
+			fwrite($myfile, "[".$title->nodeValue."](~!~)");
 	}
 	$spans = $xpath->evaluate("/html/body//span");
 	for ($i = 0; $i < $spans->length; $i++) {
 		$span = $spans->item($i);
 			
-			$textoHTML.= $span->nodeValue;
+			$textoSpan= split(" ", $span->nodeValue);
+			foreach ($textoSpan as $word) {
+				if (strlen($word)>0) {
+
+					//$textoHTML = $textoHTML."[span]"."[".$word."]";
+					fwrite($myfile,"[span]"."[".$word."](~!~)");
+
+				}
+			}
 	}
 	$ps = $xpath->evaluate("/html/body//p");
 	for ($i = 0; $i < $ps->length; $i++) {
 		$p = $ps->item($i);
 			
-			$textoHTML.= $p->nodeValue;
+			$textoP = split(" ", $p->nodeValue);
+			foreach ($textoP as $word ) {
+				if (strlen($word)>0) {
+
+					//$textoHTML = $textoHTML."[p]"."[".$word."]";
+					fwrite($myfile,"[p]"."[".$word."](~!~)");	
+				}
+			}
 	}
 
 	$h1s = $xpath->evaluate("/html/body//h1");
 	for ($i = 0; $i < $h1s->length; $i++) {
 		$h1 = $h1s->item($i);
 			
-			$textoHTML.= $h1->nodeValue;
+
+			$textoh1 = split(" ", $h1->nodeValue);
+			foreach ($textoh1 as $word) {
+				if (strlen($word)>0) {
+
+					//$textoHTML = $textoHTML."[h1]"."[".$word."]";
+					fwrite($myfile,"[h1]"."[".$word."](~!~)");	
+				}
+			}
 	}
 	$h2s = $xpath->evaluate("/html/body//h2");
 	for ($i = 0; $i < $h2s->length; $i++) {
 		$h2 = $h2s->item($i);
 			
-			$textoHTML.= $h2->nodeValue;
+			$textoh2 = split(" ", $h2->nodeValue);
+			foreach ($textoh2 as $word) {
+				if (strlen($word)>0) {
+
+					//$textoHTML = $textoHTML."[h2]"."[".$word."]";
+					fwrite($myfile,"[h2]"."[".$word."](~!~)");
+				}
+			}
 	}
 	$h3s = $xpath->evaluate("/html/body//h3");
 	for ($i = 0; $i < $h3s->length; $i++) {
 		$h3 = $h3s->item($i);
-			
-			$textoHTML.= $h3->nodeValue;
+			$textoh3 = split(" ", $h3->nodeValue);
+			foreach ($textoh3 as $word) {
+				if (strlen($word)>0) {
+
+					//$textoHTML = $textoHTML."[h3]"."[".$word."]";
+					fwrite($myfile,"[h3]"."[".$word."](~!~)");
+				}
+			}
 	}
 	$h4s = $xpath->evaluate("/html/body//h4");
 	for ($i = 0; $i < $h4s->length; $i++) {
 		$h4 = $h4s->item($i);
-			
-			$textoHTML.= $h4->nodeValue;
+			$textoh4 = split(" ", $h4->nodeValue);
+			foreach ($textoh4 as $word) {
+				if (strlen($word)>0) {
+
+					//$textoHTML = $textoHTML."[h4]"."[".$word."]";
+					fwrite($myfile,"[h4]"."[".$word."](~!~)");
+				}
+			}
 	}
 	$h5s = $xpath->evaluate("/html/body//h5");
 	for ($i = 0; $i < $h5s->length; $i++) {
 		$h5 = $h5s->item($i);
-			
-			$textoHTML.= $h5->nodeValue;
+			$textoh5 = split(" ", $h5->nodeValue);
+			foreach ($textoh5 as $word) {
+				if (strlen($word)>0) {
+
+					//$textoHTML = $textoHTML."[h5]"."[".$word."]";
+					fwrite($myfile,"[h5]"."[".$word."](~!~)");
+				}
+			}
 	}
 	$h6s = $xpath->evaluate("/html/body//h6");
 	for ($i = 0; $i < $h6s->length; $i++) {
 		$h6 = $h6s->item($i);
-			
-			$textoHTML.= $h6->nodeValue;
+			$textoh6 = split(" ", $h6->nodeValue);
+			foreach ($textoh6 as $word) {
+				if (strlen($word)>0) {
+
+					//$textoHTML = $textoHTML."[h6]"."[".$word."]";
+					fwrite($myfile,"[h6]"."[".$word."](~!~)");
+				}
+			}
 	}
 		
 		return $textoHTML;
 
 }
-
 
 
 
@@ -115,6 +172,7 @@ function get_links($url){
 			
 			if(!in_array($href->getAttribute('href'), $c)){
 				array_push($c,$href->getAttribute('href'));
+				//echo $href->getAttribute('href')."</p>";
 			}
 
 		}
@@ -142,10 +200,11 @@ function webCrawler($direccionStartLink,$timeLimit){
 }
 $contador = 0;	
 function webCrawlerAux($arrayListaWeb){
+		//echo "<h1>............................".sizeof($arrayListaWeb).".......................</h1>";
 		global $contador;
 		global $arrayHTMLS;
 		foreach ($arrayListaWeb as  $paginaWeb) {
-			//$codigoHtml = getHTML($paginaWeb);
+			$codigoHtml = getHTML($paginaWeb);
 			echo "<h5>".$contador.")".$paginaWeb."</h5>";
 			//echo $codigoHtml;
 			//array_push($arrayHTMLS, $codigoHtml);
@@ -155,7 +214,9 @@ function webCrawlerAux($arrayListaWeb){
 			unset($newFinalWebList);
 		}
 		$newFinalWebList = array();
+		//$tamanoLista = sizeof($arrayListaWeb);
 		foreach ($arrayListaWeb as $key) {
+			//echo "Voy a buscar en la pagina: ".$key;
 			$newWebList = get_links($key);
 			foreach ($newWebList as $paginita) {
 				array_push($newFinalWebList, $paginita);
@@ -168,6 +229,6 @@ function webCrawlerAux($arrayListaWeb){
 }
 
 webCrawler("listaPaginas.txt",900000000000000000000);
-
+//get_links("http://resultados.as.com/resultados/ficha/deportista/casillas/390/");
 
 ?>
